5dc0e934a94051261169a59ce50a874a30a80a8f
wangxingkang <wangxingkang@sensoro.com>
2019-05-30 15:45:32 +0800

    feat: 添加到index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { isEmail, emailRegexp } from './regexp/is-email';
 export { isIdCard, idCardRegexp } from './regexp/is-id-card';
 export { isPhone, phoneRegexp } from './regexp/is-phone';
 export { isUrl, urlRegex } from './regexp/is-url';
+export { urlToList } from './url/tools';
